ad86141b0f349f34abd33f61b97b50dbf4ede96c
Tyler Adams <tjadams.giga@gmail.com>
2020-08-26 23:13:50 -0700

    Init frontend with create-react-app

---
+++ frontend/fogblog-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World, I'm fogblog!
+    </div>
+  );
+}
+
+export default App;
